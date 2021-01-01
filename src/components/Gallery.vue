ef3962fa218c8e476bd1a743d62a416027bb11e4
burakcatan <burakcatan@gmail.com>
2021-01-01 20:33:26 +0300

    add Card component to Gallery

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,12 +1,19 @@
 <template>
-    <div class="a">
-        <h1>Gallery</h1>
-    </div>
+  <div class="gallery-wrapper">
+    <ul class="gallery">
+      <li v-for="pokemon in pokemons" :key="pokemon"><Card :pokemon="pokemon"/></li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import Card from '@/components/Card.vue';
+
 export default {
   name: 'Gallery',
+  components: {
+    Card,
+  },
   computed: {
     pokemons() {
       return this.$store.state.pokemons;
@@ -17,3 +24,30 @@ export default {
   },
 };
 </script>
+
+<style lang='scss' scoped>
+.gallery-wrapper{
+  justify-content: center;
+}
+
+ul {
+    margin-top: 20px;
+    display:grid;
+    grid-template-columns: repeat(auto-fill,minmax(350px, 1fr));
+    width: 80%;
+    margin-left:auto;
+    margin-right:auto;
+    justify-content: center;
+
+}
+li {
+  list-style: none;
+  margin-left: auto;
+  margin-right: auto;
+}
+// li{
+//     list-style: none;
+//     float:left;
+//     margin:20px;
+// }
+</style>
