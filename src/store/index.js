1853617b981225037af683af717b473f1255d88d
burakcatan <burakcatan@gmail.com>
2021-01-01 18:30:37 +0300

    add axios & fetch methods

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,19 @@
 import { createStore } from 'vuex';
+import axios from 'axios';
 
 export default createStore({
   state: {
+    pokemons: [],
   },
   mutations: {
+    setPokemons(state, payload) {
+      state.pokemons = payload;
+    },
   },
   actions: {
-  },
-  modules: {
+    async fetchPokemons({ commit }) {
+      const res = await axios.get('https://pokedex-api-flask.herokuapp.com/get/pokemons');
+      commit('setPokemons', res.data.results);
+    },
   },
 });
