65170082c63803a75c2b0f823b22e8f8cf3e236a
burakcatan <burakcatan@gmail.com>
2021-01-02 01:45:38 +0300

    add getters & filtering by type

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,18 @@ export default createStore({
   state: {
     pokemons: [],
     types: [],
+    results: [],
+  },
+  getters: {
+    getPokemons(state) {
+      return state.pokemons;
+    },
+    getResults(state) {
+      return state.results;
+    },
+    getCurrent(state, getters) {
+      return (state.results.length > 0) ? getters.getResults : getters.getPokemons;
+    },
   },
   mutations: {
     setPokemons(state, payload) {
@@ -13,9 +25,18 @@ export default createStore({
     setTypes(state, payload) {
       state.types = payload;
     },
+    addResults(state, payload) {
+      const difference = payload.filter((item) => !state.results.includes(item));
+      state.results = state.results
+        .concat(difference);
+    },
+    removeResults(state, payload) {
+      const difference = state.results.filter((item) => !payload.includes(item));
+      state.results = difference;
+    },
   },
   actions: {
-    async fetchPokemons({ commit }) {
+    async fetchAllPokemons({ commit }) {
       const res = await axios.get('https://pokedex-api-flask.herokuapp.com/get/pokemons');
       commit('setPokemons', res.data.results);
     },
@@ -24,6 +45,21 @@ export default createStore({
       const types = [];
       res.data.results.forEach((type) => types.push(type.name));
       commit('setTypes', types);
+    },
+    filterType({ state }, type) {
+      const pokemons = state.pokemons
+        .filter((pokemon) => pokemon['Type I'][0] === type)
+        .concat(state.pokemons
+          .filter((pokemon) => pokemon['Type II'] && pokemon['Type II'][0] === type));
+      return pokemons;
+    },
+    async addType(context, type) {
+      const pokemons = await context.dispatch('filterType', type);
+      context.commit('addResults', pokemons);
+    },
+    async removeType(context, type) {
+      const pokemons = await context.dispatch('filterType', type);
+      context.commit('removeResults', pokemons);
     },
   },
 });
