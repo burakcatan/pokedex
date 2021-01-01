c95372e76868efe8d244b1772e82858e92a81c4e
burakcatan <burakcatan@gmail.com>
2021-01-01 18:37:42 +0300

    Update Pokedex.vue

--- src/views/Pokedex.vue
+++ src/views/Pokedex.vue
@@ -2,4 +2,22 @@
   <div class="pokedex">
     <h1>Pokedex</h1>
   </div>
+  <Gallery />
 </template>
+
+<script>
+import Gallery from '@/components/Gallery.vue';
+
+export default {
+  components: {
+    Gallery,
+  },
+};
+</script>
+
+<style lang='scss' scoped>
+.pokedex {
+  margin-top: 100px;
+}
+
+</style>
