a3a0bb8002107eac59fba1713dbda015e5b94770
burakcatan <burakcatan@gmail.com>
2021-01-01 20:34:52 +0300

    Add search component

--- src/views/Pokedex.vue
+++ src/views/Pokedex.vue
@@ -1,23 +1,35 @@
 <template>
   <div class="pokedex">
-    <h1>Pokedex</h1>
+    <div class="headline">
+      <h1>All <b>Pokemons</b> for you to choose your favorite</h1>
+    </div>
+    <Search />
+    <Gallery />
   </div>
-  <Gallery />
 </template>
 
 <script>
 import Gallery from '@/components/Gallery.vue';
+import Search from '@/components/Search.vue';
 
 export default {
   components: {
     Gallery,
+    Search,
   },
 };
 </script>
 
 <style lang='scss' scoped>
 .pokedex {
-  margin-top: 100px;
+  padding-top: 140px;
+  padding-left: 10%;
+  padding-right: 10%;
+  text-align: center;
 }
-
+h1 {
+  font-size: 2em;
+  letter-spacing: 3px;
+  line-height: 40px;
+}
 </style>
