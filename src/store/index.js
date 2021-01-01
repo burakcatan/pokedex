e24889e5d1badbfcaddf8b4f8cbacc4c65f11ce0
burakcatan <burakcatan@gmail.com>
2021-01-01 21:02:39 +0300

    add fetchData method

--- src/store/index.js
+++ src/store/index.js
@@ -4,16 +4,26 @@ import axios from 'axios';
 export default createStore({
   state: {
     pokemons: [],
+    types: [],
   },
   mutations: {
     setPokemons(state, payload) {
       state.pokemons = payload;
+    },
+    setTypes(state, payload) {
+      state.types = payload;
     },
   },
   actions: {
     async fetchPokemons({ commit }) {
       const res = await axios.get('https://pokedex-api-flask.herokuapp.com/get/pokemons');
       commit('setPokemons', res.data.results);
+    },
+    async fetchTypes({ commit }) {
+      const res = await axios.get('https://pokedex-api-flask.herokuapp.com/get/types');
+      const types = [];
+      res.data.results.forEach((type) => types.push(type.name));
+      commit('setTypes', types);
     },
   },
 });
