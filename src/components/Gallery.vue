dfe0a1fd4508d80748350db4c71b98e4c2af2f05
burakcatan <burakcatan@gmail.com>
2021-01-01 18:37:10 +0300

    Create Gallery.vue

---
+++ src/components/Gallery.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="a">
+        <h1>Gallery</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Gallery',
+  computed: {
+    pokemons() {
+      return this.$store.state.pokemons;
+    },
+  },
+  created() {
+    this.$store.dispatch('fetchPokemons');
+  },
+};
+</script>
