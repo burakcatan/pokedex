f95df982a1aee00991317a2e4e6a73d0604f1b65
burakcatan <burakcatan@gmail.com>
2021-01-02 22:59:18 +0300

    add transition

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <Header />
-  <router-view/>
+  <transition name="slide-fade">
+    <router-view/>
+  </transition>
 </template>
 
 <script>
@@ -12,3 +14,21 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+/* Enter and leave animations can use different */
+/* durations and timing functions.              */
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
+}
+</style>
